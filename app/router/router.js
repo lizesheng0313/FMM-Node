cb3cb6838add75b26c07eab393bb58258d3595af
李泽胜 <zesheng.li@lanmaoly.com>
2023-02-23 16:14:10 +0800

    路由调整

--- app/router/router.js
+++ app/router/router.js
@@ -2,7 +2,7 @@
  * @Author: lizesheng
  * @Date: 2023-02-23 14:08:48
  * @LastEditors: lizesheng
- * @LastEditTime: 2023-02-23 15:31:18
+ * @LastEditTime: 2023-02-23 16:13:44
  * @important: 重要提醒
  * @Description: 备注内容
  * @FilePath: /commerce_egg/app/router/router.js
@@ -14,7 +14,7 @@
  */
 module.exports = app => {
   const { router, controller } = app;
-  router.post('/api/manage/addMenu', controller.menu.add);
-  router.post('/api/manage/updateMenu', controller.menu.update);
-  router.get('/api/manage/getMenu', controller.menu.get);
+  router.post('/api/management/menu/add', controller.menu.add);
+  router.post('/api/management/menu/update', controller.menu.update);
+  router.get('/api/management/menu/get', controller.menu.get);
 };
