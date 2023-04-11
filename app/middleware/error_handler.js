6f27849e296ff611a4b8641a13651618a3062b88
李泽胜 <zesheng.li@lanmaoly.com>
2023-04-11 14:40:04 +0800

    中件间调整

--- app/middleware/error_handler.js
+++ app/middleware/error_handler.js
@@ -2,7 +2,7 @@
  * @Author: lizesheng
  * @Date: 2023-02-23 15:19:42
  * @LastEditors: lizesheng
- * @LastEditTime: 2023-04-11 14:38:25
+ * @LastEditTime: 2023-04-11 14:39:57
  * @important: 重要提醒
  * @Description: 备注内容
  * @FilePath: /commerce_egg/app/middleware/error_handler.js
@@ -14,10 +14,11 @@ module.exports = () => {
     try {
       await next();
     } catch (err) {
+      console.log(err, '----err')
       const status = err?.status || 500;
       const error = status === 500 && ctx.app.config.env === 'prod'
         ? 'Internal Server Error'
-        : err.message;
+        : err?.message;
       // 所有的异常都在 app 上触发一个 error 事件，框架会记录一条错误日志
       ctx.logger.error('发生错误中间件', error);
       // 生产环境时 500 错误的详细错误内容不返回给客户端，因为可能包含敏感信息
