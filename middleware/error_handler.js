8aebb47fa6ee3ff5cdfc0fa226036f9cfb9a9f34
李泽胜 <zesheng.li@lanmaoly.com>
2023-04-11 14:13:56 +0800

    错误日志

--- middleware/error_handler.js
+++ middleware/error_handler.js
@@ -1,3 +1,12 @@
+/*
+ * @Author: lizesheng
+ * @Date: 2023-02-23 15:19:42
+ * @LastEditors: lizesheng
+ * @LastEditTime: 2023-04-11 14:13:21
+ * @important: 重要提醒
+ * @Description: 备注内容
+ * @FilePath: /commerce_egg/middleware/error_handler.js
+ */
 'use strict';
 
 module.exports = () => {
@@ -6,8 +15,8 @@ module.exports = () => {
       await next();
     } catch (err) {
       // 所有的异常都在 app 上触发一个 error 事件，框架会记录一条错误日志
+      ctx.logger.error('发生错误', err);
       ctx.app.emit('error', err, ctx);
-
       const status = err.status || 500;
       // 生产环境时 500 错误的详细错误内容不返回给客户端，因为可能包含敏感信息
       const error = status === 500 && ctx.app.config.env === 'prod'
