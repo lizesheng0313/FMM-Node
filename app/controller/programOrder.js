dde9620941c957c3163757752f76e43733700090
李泽胜 <zesheng.li@lanmaoly.com>
2023-04-11 14:58:33 +0800

    对订单详情接口调整

--- app/controller/programOrder.js
+++ app/controller/programOrder.js
@@ -2,7 +2,7 @@
  * @Author: lizesheng
  * @Date: 2023-02-23 14:08:48
  * @LastEditors: lizesheng
- * @LastEditTime: 2023-04-11 14:52:21
+ * @LastEditTime: 2023-04-11 14:58:14
  * @important: 重要提醒
  * @Description: 备注内容
  * @FilePath: /commerce_egg/app/controller/programOrder.js
@@ -235,7 +235,7 @@ class ProgramOrderController extends Controller {
       p.city,
       p.streetName,
       lo.logistics_company,
-      lo.logistics_no
+      lo.logistics_no,
       (SELECT COUNT(*) FROM goods_order) AS total
     FROM goods_order_return r
       LEFT JOIN logistics lo ON r.order_id = lo.order_id
@@ -303,7 +303,7 @@ class ProgramOrderController extends Controller {
         p.city,
         p.streetName,
         lo.logistics_company,
-        lo.logistics_no
+        lo.logistics_no,
         (SELECT COUNT(*) FROM goods_order) AS total
       FROM
         goods_order o
