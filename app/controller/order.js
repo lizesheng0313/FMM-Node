1deb1c975fb628080b6ba1b127389377675129d3
李泽胜 <zesheng.li@lanmaoly.com>
2023-04-28 17:58:59 +0800

    同意退货调整

--- app/controller/order.js
+++ app/controller/order.js
@@ -2,7 +2,7 @@
  * @Author: lizesheng
  * @Date: 2023-02-23 14:08:48
  * @LastEditors: lizesheng
- * @LastEditTime: 2023-04-28 17:53:04
+ * @LastEditTime: 2023-04-28 17:57:50
  * @important: 重要提醒
  * @Description: 备注内容
  * @FilePath: /commerce_egg/app/controller/order.js
@@ -260,7 +260,8 @@ class OrderController extends Controller {
   async goodsAgreenOperation() {
     const { ctx, app } = this;
     const { id, return_address } = ctx.request.body;
-    const order = await app.mysql.get('goods_order', { id });
+    const agreeData = await app.mysql.get('goods_order_return', { id });
+    const order = await app.mysql.get('goods_order', { id: agreeData.order_id });
 
     // 检查订单状态是否为“已完成”或“退货中”
     if (!['20', '30', '40'].includes(order.order_status)) {
