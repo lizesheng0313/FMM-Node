91a354b8ba1cfbd9bb7601daeaa4742f6582423c
lizesheng <lizesheng0313@gmail.com>
2024-07-02 15:08:06 +0800

    增加sql配置

--- config/config.default.js
+++ config/config.default.js
@@ -31,8 +31,8 @@ module.exports = (appInfo) => {
   config.mysql = {
     // 单数据库信息配置
     client: {
-      host: 'localhost',
-      // host: '101.200.188.81',
+      // host: 'localhost',
+      host: '101.200.188.81',
       // 端口号
       port: '3306',
       // 用户名
